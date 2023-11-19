8fea8e121f64f978505269d338c1044a15587a4f
Erick <ericksantiagogonzalezmarin11@gmail.com>
2023-11-19 13:32:36 -0600

    Correcion de la conexion

--- Chedraui/conexion.php
+++ Chedraui/conexion.php
@@ -6,30 +6,32 @@ $base_de_datos = "Chedraui";
 
 function conectarBaseDatos($contrasena) {
     $conexion = @new mysqli("localhost", "root", $contrasena, "Chedraui");
-    
+
     if ($conexion->connect_error) {
         // Devuelve false si la conexión falla
         return false;
     }
-    
+
     return $conexion;
 }
 
 $passwords = array("Winsome1", "Ribendiaz232");
-$conexion = mysqli_connect($host, $usuario, $contrasena, $base_de_datos);
+$conexion = false; // Cambiado el nombre de la variable a $conexion
 
 // Intentar conectar con las contraseñas
 foreach ($passwords as $password) {
-    $conn = conectarBaseDatos($password);
-    
+    $conexion = conectarBaseDatos($password);
+
     // Si la conexión es exitosa, sal del bucle
-    if ($conn) {
+    if ($conexion) {
         break;
     }
 }
 
 // Verificar si se estableció una conexión
-if (!$conn) {
+if (!$conexion) {
     die("No se pudo conectar a la base de datos.");
 }
+
+// Ahora puedes realizar consultas con $conexion
 ?>
