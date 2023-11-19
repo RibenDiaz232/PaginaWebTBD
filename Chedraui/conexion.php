956c3cc631e1a27b45755ac5aa89dd8d462fe7cf
RibenDiaz232 <adolfolopezmaster@gmail.com>
2023-11-19 13:13:56 -0600

    Paridad temporal de contraseñas, solo que pendiente la de Martín

--- Chedraui/conexion.php
+++ Chedraui/conexion.php
@@ -1,13 +1,35 @@
-
 <?php
 $host = "localhost";
 $usuario = "root";
-$contrasena = "Winsome1";
+$contrasena = null;
 $base_de_datos = "Chedraui";
 
+function conectarBaseDatos($contrasena) {
+    $conexion = @new mysqli("localhost", "root", $contrasena, "Chedraui");
+    
+    if ($conexion->connect_error) {
+        // Devuelve false si la conexión falla
+        return false;
+    }
+    
+    return $conexion;
+}
+
+$passwords = array("Winsome1", "Ribendiaz232");
 $conexion = mysqli_connect($host, $usuario, $contrasena, $base_de_datos);
 
-if (!$conexion) {
-    die("Error de conexión: " . mysqli_connect_error());
+// Intentar conectar con las contraseñas
+foreach ($passwords as $password) {
+    $conn = conectarBaseDatos($password);
+    
+    // Si la conexión es exitosa, sal del bucle
+    if ($conn) {
+        break;
+    }
+}
+
+// Verificar si se estableció una conexión
+if (!$conn) {
+    die("No se pudo conectar a la base de datos.");
 }
 ?>
