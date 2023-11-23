bc78b180b2f9e962e9dae36b1417c6cd2a839e23
Erick <ericksantiagogonzalezmarin11@gmail.com>
2023-11-23 13:08:38 -0600

    modificacion dela pagina

--- Chedraui/cliente.php
+++ Chedraui/cliente.php
@@ -1,6 +1,27 @@
 <?php
 include_once 'conexion.php';
 
+// Iniciar sesión
+session_start();
+
+// Verificar si el usuario ha iniciado sesión
+if (isset($_SESSION['usuario'])) {
+    $usuario = $_SESSION['usuario'];
+    $botonIniciarSesion = "<li class='nav-item dropdown'>
+                            <a class='nav-link dropdown-toggle' href='#' id='navbarDropdown' role='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+                                $usuario
+                            </a>
+                            <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
+                                <a class='dropdown-item' href='perfil.php'>Ver Perfil</a>
+                                <div class='dropdown-divider'></div>
+                                <a class='dropdown-item' href='logout.php'>Cerrar Sesión</a>
+                            </div>
+                          </li>";
+} else {
+    $botonIniciarSesion = "<li class='nav-item'>
+                            <a class='nav-link' href='login.php'>Iniciar Sesión</a>
+                          </li>";
+}
 
 // Verificar si se estableció una conexión
 if (!$conexion) {
@@ -75,98 +96,97 @@ $conexion->close();
     <meta charset="UTF-8">
     <title>Inicio</title>
     <!-- Agrega el enlace a los archivos de Bootstrap CSS -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <!-- Mueve el logotipo a la derecha utilizando ml-auto -->
-            <img src="../chedraui/img/oxxo-gaming.png" alt="logo" width="150px" class="ml-auto">
-            <!-- Botón de hamburguesa para dispositivos móviles -->
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Catagoria</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Promociones</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Atencion a Clientes</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contacto</a>
-                    </li>
-                </ul>
-            </div>
-            <ul class="navbar-nav ml-auto">
+    <div class="container">
+        <!-- Mueve el logotipo a la derecha utilizando ml-auto -->
+        <img src="../chedraui/img/oxxo-gaming.png" alt="logo" width="150px" class="ml-auto">
+        <!-- Botón de hamburguesa para dispositivos móviles -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="login.php">Iniciar Sesión</a>
+                    <a class="nav-link" href="index.php">Inicio</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="carrito.php">
-                        <i class="fas fa-shopping-cart"></i> Carrito
-                    </a>
+                    <a class="nav-link" href="#">Catagoria</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Promociones</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Atencion a Clientes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contacto</a>
                 </li>
             </ul>
         </div>
-    </nav>
-    <div class="container mt-5">
-        <!-- Agrega un formulario de selección de categoría -->
-        <form method="GET" action="index.php" class="mb-3">
-            <label for="categoria">Categoría:</label>
-            <select name="categoria" id="categoria" class="form-control">
-                <option value="" <?php echo ($categoriaSeleccionada == '') ? 'selected' : ''; ?>>Todas las categorías</option>
-                <?php foreach ($categorias as $categoria): ?>
-                    <option value="<?php echo $categoria; ?>" <?php echo ($categoriaSeleccionada == $categoria) ? 'selected' : ''; ?>><?php echo $categoria; ?></option>
-                <?php endforeach; ?>
-            </select>
-            <button type="submit" class="btn btn-primary mt-2">Filtrar</button>
-        </form>
+        <ul class="navbar-nav ml-auto">
+            <?php echo $botonIniciarSesion; ?>
+            <li class="nav-item">
+                <a class="nav-link" href="carrito.php">
+                    <i class="fas fa-shopping-cart"></i> Carrito
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<div class="container mt-5">
+    <!-- Agrega un formulario de selección de categoría -->
+    <form method="GET" action="index.php" class="mb-3">
+        <label for="categoria">Categoría:</label>
+        <select name="categoria" id="categoria" class="form-control">
+            <option value="" <?php echo ($categoriaSeleccionada == '') ? 'selected' : ''; ?>>Todas las categorías</option>
+            <?php foreach ($categorias as $categoria): ?>
+                <option value="<?php echo $categoria; ?>" <?php echo ($categoriaSeleccionada == $categoria) ? 'selected' : ''; ?>><?php echo $categoria; ?></option>
+            <?php endforeach; ?>
+        </select>
+        <button type="submit" class="btn btn-primary mt-2">Filtrar</button>
+    </form>
 
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-            <?php foreach ($producto as $producto): ?>
-                <div class="col">
-                    <div class="card h-100">
-                        <img src="<?php echo $producto['imagen_url']; ?>" class="card-img-top" alt="<?php echo $producto['nombre']; ?>">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $producto['nombre']; ?></h5>
-                            <p class="card-text"><?php echo $producto['descripcion']; ?></p>
-                            <p class="card-text">$<?php echo $producto['precio']; ?></p>
-                            <!-- Modifica el formulario para agregar al carrito -->
-                            <form method="GET" action="carrito.php">
-                                <input type="hidden" name="agregar" value="<?php echo $producto['idProducto']; ?>">
-                                <button type="submit" class="btn btn-primary">Agregar al Carrito</button>
-                            </form>
-                        </div>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <?php foreach ($producto as $producto): ?>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="<?php echo $producto['imagen_url']; ?>" class="card-img-top" alt="<?php echo $producto['nombre']; ?>">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $producto['nombre']; ?></h5>
+                        <p class="card-text"><?php echo $producto['descripcion']; ?></p>
+                        <p class="card-text">$<?php echo $producto['precio']; ?></p>
+                        <!-- Modifica el formulario para agregar al carrito -->
+                        <form method="GET" action="carrito.php">
+                            <input type="hidden" name="agregar" value="<?php echo $producto['idProducto']; ?>">
+                            <button type="submit" class="btn btn-primary">Agregar al Carrito</button>
+                        </form>
                     </div>
                 </div>
-            <?php endforeach; ?>
-        </div>
-
-        <!-- Agrega la paginación -->
-        <nav aria-label="Page navigation">
-            <ul class="pagination justify-content-center">
-                <?php for ($i = 1; $i <= $totalPaginas; $i++): ?>
-                    <li class="page-item <?php echo ($i == $paginaActual) ? 'active' : ''; ?>">
-                        <a class="page-link" href="?pagina=<?php echo $i; ?>"><?php echo $i; ?></a>
-                    </li>
-                <?php endfor; ?>
-            </ul>
-        </nav>
+            </div>
+        <?php endforeach; ?>
     </div>
-    <script src="chedraui/js/index.js"></script>
-    <!-- Agrega el enlace a la biblioteca Font Awesome para el icono del carrito -->
-    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-    <!-- Agrega el enlace a los archivos de Bootstrap JS (jQuery y Popper.js son necesarios) -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Agrega la paginación -->
+    <nav aria-label="Page navigation">
+        <ul class="pagination justify-content-center">
+            <?php for ($i = 1; $i <= $totalPaginas; $i++): ?>
+                <li class="page-item <?php echo ($i == $paginaActual) ? 'active' : ''; ?>">
+                    <a class="page-link" href="?pagina=<?php echo $i . ($categoriaSeleccionada ? '&categoria=' . $categoriaSeleccionada : ''); ?>"><?php echo $i; ?></a>
+                </li>
+            <?php endfor; ?>
+        </ul>
+    </nav>
+</div>
+<script src="chedraui/js/index.js"></script>
+<!-- Agrega el enlace a la biblioteca Font Awesome para el icono del carrito -->
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+<!-- Agrega el enlace a los archivos de Bootstrap JS (jQuery y Popper.js son necesarios) -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
