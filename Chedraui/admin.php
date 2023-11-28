d80856e305a66e74ce6c30672b7175d02626f79b
José_Martin <mendozaperdomojosemartin431@gmail.com>
2023-11-28 00:20:26 -0600

    ok

--- Chedraui/admin.php
+++ Chedraui/admin.php
@@ -1,10 +1,12 @@
 <?php
 
 include_once 'conexion.php';
+
 // Verificar si se estableció una conexión
 if (!$conexion) {
     die("No se pudo conectar a la base de datos.");
 }
+
 // Verificar si se ha iniciado sesión
 $botonIniciarSesion = '';
 session_start();
@@ -85,48 +87,45 @@ $conexion->close();
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="#">Tu Tienda</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar scroll</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
+                aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
+            <div class="collapse navbar-collapse" id="navbarScroll">
+                <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                     <li class="nav-item">
-                        <a class="nav-link" href="index.php">Inicio</a>
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Categoría</a>
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Link
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Ventas</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="">Usuarios</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="listadeproductos.php">Agregar los productos</a>
+                        <a class="nav-link disabled" aria-disabled="true">Link</a>
                     </li>
                 </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
             </div>
-            <form method="GET" action="index.php" class="form-inline">
-                <input class="form-control mr-sm-2 mx-auto" type="search" placeholder="Buscar categoría"
-                    aria-label="Search" name="categoria">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-            </form>
-
-            <ul class="navbar-nav ml-auto">
-                <?php echo $botonIniciarSesion; ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="carrito.php">
-                        <i class="fas fa-shopping-cart"></i> Carrito
-                    </a>
-                </li>
-            </ul>
         </div>
     </nav>
+
     <div class="container mt-5">
         <!-- Agrega un formulario de selección de categoría -->
         <form method="GET" action="admin.php" class="mb-3">
@@ -135,7 +134,8 @@ $conexion->close();
                 <option value="" <?php echo ($categoriaSeleccionada == '') ? 'selected' : ''; ?>>Todas las categorías
                 </option>
                 <?php foreach ($categorias as $categoria): ?>
-                    <option value="<?php echo $categoria; ?>" <?php echo ($categoriaSeleccionada == $categoria) ? 'selected' : ''; ?>>
+                    <option value="<?php echo $categoria; ?>"
+                        <?php echo ($categoriaSeleccionada == $categoria) ? 'selected' : ''; ?>>
                         <?php echo $categoria; ?>
                     </option>
                 <?php endforeach; ?>
@@ -161,7 +161,8 @@ $conexion->close();
                             </p>
                             <!-- Modifica el formulario para agregar al carrito -->
                             <form method="GET" action="carrito.php">
-                                <input type="hidden" name="agregar" value="<?php echo $producto['idProducto']; ?>">
+                                <input type="hidden" name="agregar"
+                                    value="<?php echo $producto['idProducto']; ?>">
                                 <button type="submit" class="btn btn-primary">Agregar al Carrito</button>
                             </form>
                         </div>
