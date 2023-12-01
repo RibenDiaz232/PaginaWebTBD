e12f83033c88a469796eb3a1ac6ab574f2b4ba26
José_Martin <mendozaperdomojosemartin431@gmail.com>
2023-12-01 00:04:15 -0600

    Midificación

--- Chedraui/ventas.php
+++ Chedraui/ventas.php
@@ -39,6 +39,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 $query = "SELECT * FROM ventas";
 $resultado = mysqli_query($conexion, $query);
 
+// Consulta para obtener las opciones de ID de usuario
+$queryUsuarios = "SELECT idusuario FROM usuario";
+$resultadoUsuarios = mysqli_query($conexion, $queryUsuarios);
+
+// Consulta para obtener las opciones de productos
+$queryProductos = "SELECT nombre FROM producto";
+$resultadoProductos = mysqli_query($conexion, $queryProductos);
+
 mysqli_close($conexion); // Cierra la conexión
 ?>
 
@@ -47,7 +55,7 @@ mysqli_close($conexion); // Cierra la conexión
 <head>
     <meta charset="UTF-8">
     <title>Registro de Ventas</title>
-    <!-- Incluye la hoja de estilo de Bootstrap -->
+    <!-- Incluye la hoja de estilo de Bootstrap 5 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
 </head>
 <body>
@@ -60,39 +68,81 @@ mysqli_close($conexion); // Cierra la conexión
         </div>
     <?php endif; ?>
 
-    <form method="post">
-        <!-- Agrega campos del formulario para la venta -->
-        <label for="id_usuario">ID Usuario:</label>
-        <input type="text" name="id_usuario" required>
+    <!-- Botón para abrir la ventana modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ventanaVenta">
+        Añadir venta
+    </button>
 
-        <label for="producto">Producto:</label>
-        <input type="text" name="producto" required>
+    <!-- Ventana modal para el formulario de venta -->
+    <div class="modal fade" id="ventanaVenta" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Añadir Venta</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form method="post">
+                        <!-- Campo para seleccionar ID de Usuario -->
+                        <div class="mb-3">
+                            <label for="id_usuario" class="form-label">ID Usuario:</label>
+                            <select class="form-select" name="id_usuario" required>
+                                <option selected disabled>Seleccione ID de Usuario</option>
+                                <?php while ($usuario = mysqli_fetch_assoc($resultadoUsuarios)): ?>
+                                    <option value="<?php echo $usuario['idusuario']; ?>"><?php echo $usuario['idusuario']; ?></option>
+                                <?php endwhile; ?>
+                            </select>
+                        </div>
 
-        <label for="cantidad">Cantidad:</label>
-        <input type="number" name="cantidad" required>
+                        <!-- Campo para seleccionar Producto -->
+                        <div class="mb-3">
+                            <label for="producto" class="form-label">Producto:</label>
+                            <select class="form-select" name="producto" required>
+                                <option selected disabled>Seleccione Producto</option>
+                                <?php while ($producto = mysqli_fetch_assoc($resultadoProductos)): ?>
+                                    <option value="<?php echo $producto['nombre']; ?>"><?php echo $producto['nombre']; ?></option>
+                                <?php endwhile; ?>
+                            </select>
+                        </div>
 
-        <label for="precio_unitario">Precio Unitario:</label>
-        <input type="text" name="precio_unitario" required>
+                        <!-- Campo para ingresar Cantidad -->
+                        <div class="mb-3">
+                            <label for="cantidad" class="form-label">Cantidad:</label>
+                            <input type="number" class="form-control" name="cantidad" required>
+                        </div>
 
-        <button type="submit">Registrar Venta</button>
-    </form>
+                        <!-- Campo para ingresar Precio Unitario -->
+                        <div class="mb-3">
+                            <label for="precio_unitario" class="form-label">Precio Unitario:</label>
+                            <div class="input-group">
+                                <span class="input-group-text">$</span>
+                                <input type="number" class="form-control" name="precio_unitario" required>
+                            </div>
+                        </div>
 
-    <table class="table table-striped">
-        <thead>
+                        <button type="submit" class="btn btn-primary">Registrar Venta</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <table class="table table-bordered mt-4">
+        <thead class="table-primary">
         <tr>
-            <th>ID</th>
-            <th>ID Usuario</th>
-            <th>Fecha</th>
-            <th>Producto</th>
-            <th>Cantidad</th>
-            <th>Precio Unitario</th>
-            <th>Total</th>
+            <th scope="col">#</th>
+            <th scope="col">ID Usuario</th>
+            <th scope="col">Fecha</th>
+            <th scope="col">Producto</th>
+            <th scope="col">Cantidad</th>
+            <th scope="col">Precio Unitario</th>
+            <th scope="col">Total</th>
         </tr>
         </thead>
         <tbody>
         <?php while ($venta = mysqli_fetch_assoc($resultado)): ?>
             <tr>
-                <td><?php echo $venta['id']; ?></td>
+                <th scope="row"><?php echo $venta['id']; ?></th>
                 <td><?php echo $venta['id_usuario']; ?></td>
                 <td><?php echo $venta['fecha']; ?></td>
                 <td><?php echo $venta['producto']; ?></td>
@@ -104,5 +154,8 @@ mysqli_close($conexion); // Cierra la conexión
         </tbody>
     </table>
 </main>
+
+<!-- Scripts de Bootstrap 5 -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
