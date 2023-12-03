7ed061616479fec0be7ed17bee55115a0c2147ce
Erick <ericksantiagogonzalezmarin11@gmail.com>
2023-12-03 16:13:42 -0600

    ESTA VIVO.JPG

--- Chedraui/index.php
+++ Chedraui/index.php
@@ -92,7 +92,9 @@ $conexion->close();
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="#">Tu Tienda</a>
+    <a class="navbar-brand" href="#">
+            <img src="../Chedraui/img/oxxo-gaming.png" alt="logo" width="150px">
+    </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
