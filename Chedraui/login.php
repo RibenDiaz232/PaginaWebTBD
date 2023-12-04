bbd10843f983c77c4c0bf055b01fbb7de0a2488c
RibenDiaz232 <adolfolopezmaster@gmail.com>
2023-12-03 23:20:11 -0600

    Login adpatado al español porque sí :v

--- Chedraui/login.php
+++ Chedraui/login.php
@@ -17,7 +17,7 @@
 			<div class="row full-height justify-content-center">
 				<div class="col-12 text-center align-self-center py-5">
 				<div class="section pb-5 pt-5 pt-sm-2 text-center">
-						<h6 class="mb-0 pb-3"><span>Log In </span><span>Sign Up</span></h6>
+						<h6 class="mb-0 pb-3"><span>Inciar Sesión</span><span>Registrarse</span></h6>
 			          	<input class="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
 			          	<label for="reg-log"></label>
 						<div class="card-3d-wrap mx-auto">
@@ -25,46 +25,48 @@
 								<div class="card-front">
 									<div class="center-wrap">
 										<div class="section text-center">
-											<h4 class="mb-4 pb-3">Log In</h4>
+											<h4 class="mb-4 pb-3">Ingrese sus datos para iniciar sesión</h4>
 											<form method="post" action="validacion.php"> 
 											<div class="form-group">
-												<input type=7y"email" class="form-style" name="email" placeholder="Email">
+												<input type=7y"email" class="form-style" name="email" placeholder="Correo Electrónico">
 												<i class="input-icon uil uil-at"></i>
 											</div>	
 											<div class="form-group mt-2">
-												<input type="password" class="form-style" name="password" placeholder="Password">
+												<input type="password" class="form-style" name="password" placeholder="Contraseña">
 												<i class="input-icon uil uil-lock-alt"></i>
 											</div>
 											<input  type="submit" value="Login" name="login" class="btn mt-4">
 											
 											</form>
-                      <p class="mb-0 mt-4 text-center"><a href="#" class="link">Forgot your password?</a></p>
+                      <p class="mb-0 mt-4 text-center"><a href="#" class="link">¿Olvidaste tu contraseña?</a></p>
+					  <p class="mb-0 mt-4 text-center"><a href="index.php" class="link">Regresar al menú principal</a></p>
 				      					</div>
 			      					</div>
 			      				</div>
 								<div class="card-back">
 									<div class="center-wrap">
 										<div class="section text-center">
-											<h4 class="mb-3 pb-3">Sign Up</h4>
+											<h4 class="mb-3 pb-3">Registrese para crear una cuenta</h4>
 											<form method="post">
 											<div class="form-group">
-												<input type="text" class="form-style" name="nombre-completo" placeholder="Full Name">
+												<input type="text" class="form-style" name="nombre-completo" placeholder="Nombre completo">
 												<i class="input-icon uil uil-user"></i>
 											</div>	
 											<div class="form-group mt-2">
-												<input type="tel" class="form-style" name="numero-de-telefono" placeholder="Phone Number">
+												<input type="tel" class="form-style" name="numero-de-telefono" placeholder="Número telefónico">
 												<i class="input-icon uil uil-phone"></i>
 											</div>	
                       <div class="form-group mt-2">
-												<input type="email" class="form-style" name="email" placeholder="Email">
+												<input type="email" class="form-style" name="email" placeholder="Correo Electrónico">
 												<i class="input-icon uil uil-at"></i>
 											</div>
 											<div class="form-group mt-2">
-												<input type="password" class="form-style" name="password" placeholder="Password">
+												<input type="password" class="form-style" name="password" placeholder="Contraseña">
 												<i class="input-icon uil uil-lock-alt"></i>
 											</div>
-											<input  type="submit" value="Register" name="register" class="btn mt-4">
+											<input  type="submit" value="Registrarse" name="register" class="btn mt-4">
 											</form>
+											<p class="mb-0 mt-4 text-center"><a href="index.php" class="link">Regresar al menú principal</a></p>
 				      					</div>
 			      					</div>
 			      				</div>
